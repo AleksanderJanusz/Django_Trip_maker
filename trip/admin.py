e93a0c2a9bf4ac0252c0358593d6886171f322e7
AleksanderJanusz <oleksanderjanusz@gmail.com>
2023-05-20 16:19:43 +0200

    models made and tested by admin

--- trip/admin.py
+++ trip/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from trip.models import *
 
-# Register your models here.
+admin.site.register(Travel)
+admin.site.register(Place)
+admin.site.register(Attraction)
+admin.site.register(PlaceAttraction)
+admin.site.register(Days)
+admin.site.register(TravelNotes)
+admin.site.register(Cost)
--- trip/models.py
+++ trip/models.py
@@ -1,3 +1,74 @@
 from django.db import models
 
+
 # Create your models here.
+
+class TravelNotes(models.Model):
+    GENRE_CHOICES = (
+        (0, 'before the travel'),
+        (1, 'during the travel'),
+        (2, 'after the travel'),
+    )
+    note = models.TextField()
+    date = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.IntegerField(choices=GENRE_CHOICES, default=0)
+    trip = models.ForeignKey('Travel', on_delete=models.CASCADE)
+
+
+class Travel(models.Model):
+    GENRE_CHOICES = (
+        (0, 'planing the travel'),
+        (1, 'ready to go'),
+        (2, 'I am back'),
+    )
+    name = models.CharField(max_length=255)
+    status = models.IntegerField(choices=GENRE_CHOICES, default=0)
+    place_attraction = models.ManyToManyField('PlaceAttraction', through='Days')
+
+    def __str__(self):
+        return self.name
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    attraction = models.ManyToManyField('Attraction', through='PlaceAttraction')
+
+    def __str__(self):
+        return self.name
+
+
+class Attraction(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time = models.CharField(null=True, max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Cost(models.Model):
+    persons = models.IntegerField(default=1)
+    cost = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    attraction = models.ForeignKey(Attraction, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.cost)
+
+
+class PlaceAttraction(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    attraction = models.ForeignKey(Attraction, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.place.name} - {self.attraction.name}'
+
+
+class Days(models.Model):
+    order = models.IntegerField(default=1)
+    place_attraction = models.ForeignKey(PlaceAttraction, on_delete=models.CASCADE)
+    travel = models.ForeignKey(Travel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.order)
